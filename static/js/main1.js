050c00307bc2f33847b7fda0181aa1f9fb26d0d7
Izk-123 <109396886+Izk-123@users.noreply.github.com>
2026-04-25 12:02:37 +0200

    Add files via upload

--- static/js/main1.js
+++ static/js/main1.js
@@ -1,71 +1,76 @@
 (function ($) {
     "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
+    // Spinner – hide when the whole page has loaded
+    $(window).on('load', function () {
+        $('#spinner').removeClass('show');
+    });
+
     // Initiate the wowjs
     new WOW().init();
 
+    // Sticky Navbar & Back-to-top – throttled with requestAnimationFrame
+    var ticking = false;
+    $(window).on('scroll', function () {
+        if (!ticking) {
+            window.requestAnimationFrame(function () {
+                var scrollTop = $(window).scrollTop() || 0;
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
+                // Sticky navbar
+                if (scrollTop > 45) {
+                    $('.navbar').addClass('sticky-top shadow-sm');
+                } else {
+                    $('.navbar').removeClass('sticky-top shadow-sm');
+                }
+
+                // Back-to-top visibility
+                if (scrollTop > 100) {
+                    $('.back-to-top').fadeIn('slow');
+                } else {
+                    $('.back-to-top').fadeOut('slow');
+                }
+
+                ticking = false;
+            });
+            ticking = true;
         }
     });
-    
-    
-    // Dropdown on mouse hover
+
+    // Dropdown on mouse hover – desktop only, with resize debounce
     const $dropdown = $(".dropdown");
     const $dropdownToggle = $(".dropdown-toggle");
     const $dropdownMenu = $(".dropdown-menu");
     const showClass = "show";
-    
-    $(window).on("load resize", function() {
-        if (this.matchMedia("(min-width: 992px)").matches) {
-            $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
-            }
-            );
-        } else {
-            $dropdown.off("mouseenter mouseleave");
-        }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-    
-})(jQuery);
+    var resizeTimer;
 
+    $(window).on("load resize", function () {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function () {
+            if (window.matchMedia("(min-width: 992px)").matches) {
+                $dropdown.hover(
+                    function () {
+                        const $this = $(this);
+                        $this.addClass(showClass);
+                        $this.find($dropdownToggle).attr("aria-expanded", "true");
+                        $this.find($dropdownMenu).addClass(showClass);
+                    },
+                    function () {
+                        const $this = $(this);
+                        $this.removeClass(showClass);
+                        $this.find($dropdownToggle).attr("aria-expanded", "false");
+                        $this.find($dropdownMenu).removeClass(showClass);
+                    }
+                );
+            } else {
+                $dropdown.off("mouseenter mouseleave");
+            }
+        }, 200); // 200ms debounce
+    });
+
+    // Back to top button – native smooth scroll
+    $('.back-to-top').click(function (e) {
+        e.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+})(jQuery);
